97dca1c9cdce8407fbd3898fbaf3c43f3444b0e1
korthaj <korthaj@users.noreply.github.com>
2018-03-13 10:28:32 +0100

    Add python example

---
+++ ovn1/uppg2.py
@@ -0,0 +1,13 @@
+# Nisse Nilsson, grudat18 uppg 1.2
+
+def odd(n):
+	"""Tell if n is an odd number."""
+	return n%2 != 0
+
+# Unit test
+assert not odd(-2)
+assert odd(-1)
+assert not odd(0)
+assert odd(1)
+assert not odd(2)
+
